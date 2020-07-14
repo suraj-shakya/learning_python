5c899d9915d785783fc7fd3fc9a89601dcce0567
suraj-shakya <envphobia@gmail.com>
2020-07-14 12:45:04 +0545

    Day 7

---
+++ Basics/helloworld.py
@@ -0,0 +1,13 @@
+r"""
+	This is  a  multiline  document string
+	Just prints hello world
+	\n
+"""
+
+
+def printHelloWorld():
+	"""
+		This is a printHelloWorld
+		will print hello world
+	"""
+	print("Hello World")
